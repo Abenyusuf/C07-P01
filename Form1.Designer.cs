8f31dd54cbbaf93c684cc4f20072398ec6710ca8
abeny <abeny@AHMEDBENYUSUF>
2024-11-19 15:12:22 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace C07_P01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OutputListbox = new System.Windows.Forms.ListBox();
+            this.TotalTextBox = new System.Windows.Forms.TextBox();
+            this.AvgTextBox = new System.Windows.Forms.TextBox();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.GetSalesButton = new System.Windows.Forms.Button();
+            this.AvgTextLabel = new System.Windows.Forms.Label();
+            this.TotalTextLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OutputListbox
+            // 
+            this.OutputListbox.FormattingEnabled = true;
+            this.OutputListbox.Location = new System.Drawing.Point(24, 8);
+            this.OutputListbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.OutputListbox.Name = "OutputListbox";
+            this.OutputListbox.Size = new System.Drawing.Size(151, 108);
+            this.OutputListbox.TabIndex = 0;
+            // 
+            // TotalTextBox
+            // 
+            this.TotalTextBox.Location = new System.Drawing.Point(92, 173);
+            this.TotalTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TotalTextBox.Name = "TotalTextBox";
+            this.TotalTextBox.Size = new System.Drawing.Size(68, 20);
+            this.TotalTextBox.TabIndex = 1;
+            // 
+            // AvgTextBox
+            // 
+            this.AvgTextBox.Location = new System.Drawing.Point(92, 131);
+            this.AvgTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.AvgTextBox.Name = "AvgTextBox";
+            this.AvgTextBox.Size = new System.Drawing.Size(68, 20);
+            this.AvgTextBox.TabIndex = 2;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(11, 201);
+            this.ExitButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(59, 35);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // GetSalesButton
+            // 
+            this.GetSalesButton.Location = new System.Drawing.Point(131, 201);
+            this.GetSalesButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.GetSalesButton.Name = "GetSalesButton";
+            this.GetSalesButton.Size = new System.Drawing.Size(59, 35);
+            this.GetSalesButton.TabIndex = 4;
+            this.GetSalesButton.Text = "Get Sales";
+            this.GetSalesButton.UseVisualStyleBackColor = true;
+            this.GetSalesButton.Click += new System.EventHandler(this.GetSalesButton_Click);
+            // 
+            // AvgTextLabel
+            // 
+            this.AvgTextLabel.AutoSize = true;
+            this.AvgTextLabel.Location = new System.Drawing.Point(8, 134);
+            this.AvgTextLabel.Name = "AvgTextLabel";
+            this.AvgTextLabel.Size = new System.Drawing.Size(79, 13);
+            this.AvgTextLabel.TabIndex = 5;
+            this.AvgTextLabel.Text = "Average Sales:";
+            // 
+            // TotalTextLabel
+            // 
+            this.TotalTextLabel.AutoSize = true;
+            this.TotalTextLabel.Location = new System.Drawing.Point(24, 176);
+            this.TotalTextLabel.Name = "TotalTextLabel";
+            this.TotalTextLabel.Size = new System.Drawing.Size(66, 13);
+            this.TotalTextLabel.TabIndex = 6;
+            this.TotalTextLabel.Text = "Total Sales :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 243);
+            this.Controls.Add(this.TotalTextLabel);
+            this.Controls.Add(this.AvgTextLabel);
+            this.Controls.Add(this.GetSalesButton);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.AvgTextBox);
+            this.Controls.Add(this.TotalTextBox);
+            this.Controls.Add(this.OutputListbox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Sales Display";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox OutputListbox;
+        private System.Windows.Forms.TextBox TotalTextBox;
+        private System.Windows.Forms.TextBox AvgTextBox;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button GetSalesButton;
+        private System.Windows.Forms.Label AvgTextLabel;
+        private System.Windows.Forms.Label TotalTextLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿//Name: Ahmed Benyusf
+//Class and Section (CS 313 01)
+//Assignment (Program 01 Chapter 07)
+//Description of the Program : this program reads sales values from a file and displays
+//the values in a list box. It also calculates the total sales and the average sales.
+
+//Extra Credit: The program also displays the highest sales value in a message box.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C07_P01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadSales(List<double> SalesList)
+        {
+            try
+            {
+                StreamReader inputFile = File.OpenText("Sales.txt");
+                //  Open the file
+                while (!inputFile.EndOfStream)
+                //  Continue processing until the end of the file is reached
+                {
+
+                    string line = inputFile.ReadLine();
+                    //  Read a line from the file
+                    if (double.TryParse(line, out double salesValue))
+                    //  Convert the input to a double
+                    {
+                        SalesList.Add(salesValue);
+                        //  Add the sales value to the list
+                    }
+                }
+                inputFile.Close();
+                //  Close the file
+            }
+            catch (Exception ex)
+            //  Catch any exceptions that occur
+            {
+                MessageBox.Show(ex.Message);
+                //  Display an error message
+            }
+        }
+
+        private void DisplaySales(List<double> SalesList)
+        {
+            foreach (double value in SalesList)
+
+            {
+                OutputListbox.Items.Add(value);
+                //  Display the sales values
+            }
+        }
+        private double AverageSales(List<double> SalesList)
+        {
+            double total = 0;
+            double Average = 0;
+            //  Set the accumulator to 0
+
+            foreach (double value in SalesList)
+
+            {
+                total += value;
+                //  Add the value to the total
+            }
+            Average = (double)total / SalesList.Count;
+            //  Calculate the average sales
+            return Average;
+            
+        }
+
+        private void TotalSales(List<double> SalesList)
+        {
+            double total = 0;
+            //  Set the accumulator to 0
+            foreach (double value in SalesList)
+            //  Add the value to the total
+            {
+                total += value;
+                //  Display the total sales
+            }
+            TotalTextBox.Text = total.ToString("c");
+            // Display the total sales
+        }
+        // extra credit
+        private void HighestSale(List<double> SalesList)
+        {
+            double Highest = SalesList.Max();
+            //  Find the largest sale
+            MessageBox.Show ("The Highest sales value is :"+Highest.ToString("c"));
+            //  Display the Highest sale
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            // Close the form.
+        }
+
+        private void GetSalesButton_Click(object sender, EventArgs e)
+        {
+            // Create a list to hold the sales values.
+            List<double> SalesList = new List<double>();
+            
+            // Read the sales values from the file
+            ReadSales(SalesList);
+            // Display the sales values.
+            DisplaySales(SalesList);
+            // Display the total sales
+            TotalSales(SalesList);
+            // Display the largest sale
+            HighestSale(SalesList);
+
+            AvgTextBox.Text = AverageSales(SalesList).ToString("c");
+            // Display the average sales
+            
+
+        }
+    }
+}
